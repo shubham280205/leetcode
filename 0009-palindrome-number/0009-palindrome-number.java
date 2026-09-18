3ddf110c6001ea93a6abe02fa24327337dfa7818
shubham vashistha <shubhamvashistha28@gmail.com>
2026-09-18 14:40:09 +0530

    Time: 5 ms (83.16%), Space: 45.6 MB (93.57%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        int o=x;
+        int rev=0;
+        while(x!=0){
+            int digit=x%10;
+            rev=rev*10+digit;
+            x/=10;
+       
+            
+             
+        
+        }
+        if(rev==o) return true;
+        
+        else return false;
+       
+         
+        
+    }
+}
